c3edae90c0868c22240f770a46b3415de893d3b6
sai3010 <saiprasanth2007@gmail.com>
2020-11-25 15:53:46 +0530

    added mem-caching using node cache

--- index.js
+++ index.js
@@ -2,14 +2,28 @@ const express = require('express')
 const app = express()
 const port = 3000
 const Utils = require('./utils')
+const NodeCache = require( "node-cache" );
+const Cache = new NodeCache({checkperiod: 120});
 app.use(express.json())
 
 app.post('/get_info',async (req, res) => {
   url = req.body.url
   const Util = new Utils.Utils()
   let stringHTML =await Util.download_content(url)
-  const parsedjson = await Util.parseHTML(stringHTML)
-  res.send(parsedjson)
+  if(Cache.get(url) == undefined)
+  {
+    
+    const parsedjson = await Util.parseHTML(stringHTML)
+    Cache.set(url,parsedjson)
+    parsedjson['fromCache'] = false
+    res.send(parsedjson)
+  }
+  else
+  {
+    const parsedjson = Cache.get(url)
+    parsedjson['fromCache'] = true
+    res.send(parsedjson)
+  }
 })
 
 app.listen(port, () => {
